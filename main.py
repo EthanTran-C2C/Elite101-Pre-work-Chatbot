81b2f62427e054ecb9667484fbe1efb78d269dfe
EthanTran-C2C <ethanrtran.us@gmail.com>
2025-01-16 23:24:54 -0600

    Update main.py
    I added additional functionalities for holding conversations

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print("Welcome to the chatbot!")
-name = input("What is your name?")
-age = input("Hello " + name + ", how old are you?")
+print("My name is Ethan")
+name = input("What is your name? ")
+age = input("Hello " + name + ", how old are you? ")
+
+while True:
+  prompt_user = input("Do you want to continue the conversation? Choose yes or no: ")
+  if prompt_user == "yes":
+    run = True
+  else:
+    run = False
+    print("Thanks for chatting!")
+    break
+  user_input = input("What do you want to learn about? I know a lot about geography or history")
+
+  if user_input = "geography":
+    print("Cool Geography fact")
+  if user_input = "history":
+    print("Cool history fact")
+    
