3231fdbffa4322ba6417919d7fe31b420cc31cfd
EthanTran-C2C <ethanrtran.us@gmail.com>
2025-01-17 00:16:16 -0600

    Update main.py
    Added additional talking prompts

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ while True:
     run = True
   else:
     run = False
-    print("Thanks for chatting!")
+    print("Thanks for chatting " + name + "!")
     break
-  user_input = input("What do you want to learn about? I know a lot about geography or history ")
+  user_input = input("What do you want to learn about? Here are some options: Geography, history, food, math ")
 
   if user_input == "geography":
     print("Did you know that Antartica is the largest desert?")
     
   if user_input == "history":
     print("Did you know that the Credit Mobilier was a fraudulent railroad company that was involved in political corruption during the construction of the Union Pacific Railroad? ")
+
+  if user_input == "food":
+    print("Did you know that a gram of protein has 4 calories?")
+
+  if user_input == "math":
+    print("Did you know that Calculus is not fun?")
     
     
     
